8b420e2ea4f900578fe9b4a1dddf4170a2540150
reger-men <adil-lashab@hotmail.com>
2018-04-17 01:31:53 +0200

    Expand the response messages

--- responses.js
+++ responses.js
@@ -1,7 +1,15 @@
 module.exports = {
   "en-US": {
     "NO_RESULTS_FOUND": "{0} did not return any results on YouTube.",
+    "NO_VIDEO_QUERY_DEFINED": "Please describe in your request which titles you would like to play.",
+    "NO_PLIST_QUERY_DEFINED": "Please describe in your request which Playlist you would like to play.",
+    "NO_PLIST_FOUND": "No Playlist found.",
     "NOW_PLAYING": "I am now playing {0}.",
+    "ADD_TO_PLAYLIST": "Song has been added to the PlayList {0}.",
+    "ALREADY_IN_PLAYLIST": "Song has already been added to PlayList {0}.",
+    "REMOVE_FROM_PLAYLIST": "Song was already removed from the PlayList.",
+    "PLAYLIST_REMOVED": "PlayList {0} has been removed.",
+    "NOT_IN_PLAYLIST": "Song was not found in the PlayList.",
     "NOTHING_TO_RESUME": "You are not playing anything currently.",
     "NOTHING_TO_REPEAT": "You have not selected a video to play.",
     "LOOP_ON_TRIGGERED": "I will repeat your {0} selection infinitely.",
@@ -11,8 +19,15 @@ module.exports = {
   },
   "de-DE": {
     "NO_RESULTS_FOUND": "Keine Ergebnisse auf Youtube gefunden.",
-    "NO_QUERY_DEFINED": "Bitte nennen Sie in Ihrer Anfrage, welche Titel Sie hören möchten.",
+    "NO_VIDEO_QUERY_DEFINED": "Bitte nennen Sie in Ihrer Anfrage, welche Titel Sie hören möchten.",
+    "NO_PLIST_QUERY_DEFINED": "Bitte nennen Sie in Ihrer Anfrage, welche Playlist Sie abspielen möchten.",
+    "NO_PLIST_FOUND": "Keine Playlist gefunden.",
     "NOW_PLAYING": "Ich spiele jetzt {0}.",
+    "ADD_TO_PLAYLIST": "Lied wurde in der PlayList {0} eingefügt.",
+    "ALREADY_IN_PLAYLIST": "Lied wurde bereits in der PlayList {0} eingefügt.",
+    "REMOVE_FROM_PLAYLIST": "Lied wurde von der PlayList entfernt.",
+    "PLAYLIST_REMOVED": "PlayList {0} wurde entfernt.",
+    "NOT_IN_PLAYLIST": "Lied wurde nicht in der PlayList gefunden.",
     "NOTHING_TO_RESUME": "Sie spielen derzeit nichts.",
     "NOTHING_TO_REPEAT": "Sie haben kein Video zum Abspielen ausgewählt.",
     "LOOP_ON_TRIGGERED": "Ich werde deine Auswahl unendlich wiederholen.",
@@ -20,4 +35,4 @@ module.exports = {
     "REPEAT_TRIGGERED": "Ich werde deine Auswahl einmal wiederholen.",
     "HELP_TRIGGERED": "Um die YouTube-Fertigkeit zu verwenden, musst du die Fähigkeit angeben, nach dem gewünschten Video zu suchen. Wenn das Video abgespielt wird, können Sie Alexa außerdem mitteilen, dass es angehalten, neu gestartet oder wiederholt werden soll."
   }
-}
+};
